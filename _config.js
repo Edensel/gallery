cfd4b4f4428f7b9d5a975dc355a0d148a4e43205
Densel Esekon <esekondensel@gmail.com>
2024-09-23 22:12:26 +0300

    Update the database name

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {};
 
 // Update to have your correct username, password, and database
 config.mongoURI = {
-    production: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.nettest?retryWrites=true&w=majority',
+    production: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/test?retryWrites=true&w=majority',
     development: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/test?retryWrites=true&w=majority',
     test: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/test?retryWrites=true&w=majority'
 };
