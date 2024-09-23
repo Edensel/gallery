cd567da84bedff0752ac22497be21caf52ed888f
Densel Esekon <esekondensel@gmail.com>
2024-09-23 23:44:24 +0300

    Updated mongoDB url

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('./config');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
@@ -10,32 +11,46 @@ let image = require('./routes/image');
 // Initializing the app
 const app = express();
 
-// Connecting the database
-let MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[process.env.NODE_ENV || 'production'];
+// connecting the database
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Database connected successfully'))
-    .catch(err => console.log("MongoDB connection error: ", err));
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
+});
+
+// test if the database has connected successfully
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
+
+
+
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder
+// Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body parser middleware
-app.use(express.json());
+// body parser middleware
+app.use(express.json())
 
-// Define routes
+
 app.use('/', index);
 app.use('/image', image);
 
-// Server listening on port
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '0.0.0.0';
 
-app.listen(PORT, HOST, () => {
-    console.log(`Server is listening on http://${HOST}:${PORT}`);
+
+ 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() =>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
 
 module.exports = app;
