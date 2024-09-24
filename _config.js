a968efc566108c514a2a20e5674312b4e313361c
Densel Esekon <esekondensel@gmail.com>
2024-09-24 22:02:03 +0300

    Updated database name

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {};
 
 // Update to have your correct username, password, and database
 config.mongoURI = {
-    production: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/?retryWrites=true&w=majority&appName=ProjectIP1Cluster',
-    development: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/?retryWrites=true&w=majority&appName=ProjectIP1Cluster',
-    test: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/?retryWrites=true&w=majority&appName=ProjectIP1Cluster'
+    production: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/gallery?retryWrites=true&w=majority&appName=ProjectIP1Cluster',
+    development: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/gallery?retryWrites=true&w=majority&appName=ProjectIP1Cluster',
+    test: 'mongodb+srv://esekondensel:FDg9JCWHnqLXOe3N@projectip1cluster.rybsw.mongodb.net/gallery?retryWrites=true&w=majority&appName=ProjectIP1Cluster'
 };
 
 module.exports = config;
